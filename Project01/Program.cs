ca102282fd0e2959044dbc06c2d49a9d2b518281
Oleg_bul <kokseg63@gmail.com>
2022-10-06 22:51:57 +0400

    final

--- Project01/Program.cs
+++ Project01/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+
+int matrX = ReadData("Ввведите размер M ");
+int matrY = ReadData("Введите размер N ");
+int[,] matrix = new int[matrX, matrY];
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrX; i++)
+    {
+      for (int j = 0; j < matrY; j++)
+        matrix [i,j] = i+j;
+    }
+}
+FillArray(matrix);
+PrintArray(matrix);
---
+++ Project03/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача сумма чисел по диагонали
+int rows=7;
+int columns=5;
+int len = rows < columns ? rows : columns;//тоже самое что if
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+int sum = 0;
+for (int i = 0; i < len; i++) 
+{
+    sum += array[i,i];
+}
+System.Console.WriteLine("Готовый вариант");
+PrintArray(array);
+
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1,10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}\t"); // \t делает ровные столбцы
+        }
+        Console.WriteLine();
+    }
+}
+System.Console.WriteLine(sum + " Сумма чисел по диагонали");
+//double = a GetRandomNumber(11,215)
+// double GetRandomNumber(double minimum, double maximum)
+// {
+//     Random random = new Random();
+//     return Math.Round(random.NextDouble() * (maximum - minimum) + minimum,2);
+// }
+// double GetRandomNumber(double minimum, double maximum)
+// {
+//     return Math.Round(new Random().NextDouble() * (maximum - minimum) + minimum,2);
+// }
